4720404c0944be07c02de699135dc901b0a2ff73
lelle1000 <linusestrandberg@gmail.com>
2026-05-13 16:39:43 +0200

    Started working and understanding the dataset, collected the average score from one player

---
+++ LinusS/linus.js
@@ -0,0 +1,32 @@
+let sameParticipantFightingScores = seasons.filter(obj => {
+    let averagePlayerScore = 0;
+    let playerId = 178;
+    let playerArr = [];
+    let totalScore;
+    if (obj.year == 0) {
+        let correctCompDay = obj.competitionDays
+        for(let day of correctCompDay) {
+
+            let events = day.events
+            let correctEvents = events.filter(event => event.disciplineId == 5)
+            
+            let samePlayerScores = 0;
+            
+            for (let event of correctEvents) {
+                let allEventScores = event.scores
+                for(let player of allEventScores) {
+                    
+                    if(player.participantId == playerId) {
+                        playerArr.push(player)
+                        totalScore = averagePlayerScore += player.score 
+
+                    
+                    }
+                }
+                 
+            }
+        }
+        console.log(totalScore / playerArr.length);
+        
+    }
+})
