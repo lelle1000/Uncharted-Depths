7d39c760ea95c47a31184155ec3b526ce61c3f3a
sarakarlssson <sara.amanda.karlsson@gmail.com>
2026-05-18 14:51:58 +0200

    Updates

--- SaraK/saraK.js
+++ SaraK/saraK.js
@@ -1,7 +1,6 @@
 const backArrow = document.getElementById("backArrow")
 const main = document.querySelector("main")
 
-
 const containerAllMonstersBox = document.querySelectorAll("#containerAllMonstersBox .monsterCard");
 const clearSelectionButton = document.getElementById("clearSelectionButton")
 const selectRandomButton = document.getElementById("selectRandomButton")
@@ -28,55 +27,7 @@ containerAllMonstersBox.forEach(card => { ///  Select card den du klickar på
 
 backArrow.addEventListener("click", () => { // gå tillbaka till landing page
     console.log("Gå till Landing page ->")
-    document.body.innerHTML = `
-        <main id="landingPageContainer">
-        <div id="topSectionLandingPage">
-            <div id="topCreaturesContainer">
-                    
-            </div>
-            <div id="topCreatureStats">
 
-            </div>
-        </div>
-
-        <div id="bottomSectionLandingPage">
-            <div id="scoreboardContainer">
-                <div id="disciplineButtons">
-                    <button class="disciplineButton" id="fightingDiscipline" data-disciplineid="5">Fighting</button>
-                    <button class="disciplineButton" id="raceDiscipline" data-disciplineid="4">Race</button>
-                    <button class="disciplineButton" id="hidenseekDiscipline" data-disciplineid="3">Hide n Seek</button>
-                    <button class="disciplineButton" id="mazeDiscipline" data-disciplineid="1">Maze</button>
-                    <button class="disciplineButton" id="huntDiscipline" data-disciplineid="2">Hunt</button>
-                    <button class="disciplineButton" id="calcRatesButton">Calculate Rates</button>
-                </div>
-                <div id="scoreboardBottomSection">
-                    <div id="scoreboardSeasonContainer">
-                        <p>Seasons</p>
-                        <button class="seasonButton" id="season1Landing" data-currentseason="1">1</button>
-                        <button class="seasonButton" id="season2Landing" data-currentseason="2">2</button>
-                        <button class="seasonButton" id="season3Landing" data-currentseason="3">3</button>
-                        <button class="seasonButton" id="season4Landing" data-currentseason="4">4</button>
-                        <button class="seasonButton" id="season5Landing" data-currentseason="5">5</button>
-                        <button class="seasonButton" id="season6Landing" data-currentseason="6">6</button>
-                        <button class="seasonButton" id="season7Landing" data-currentseason="7">7</button>
-                        <button class="seasonButton" id="season8Landing" data-currentseason="8">8</button>
-                        <button class="seasonButton" id="season9Landing" data-currentseason="9">9</button>
-                        <button class="seasonButton" id="season10Landing" data-currentseason="10">10</button>
-                    </div>
-                    <div id="scoreboardStatsContainer">
-                        <div id="scoreboardListingTitles">
-                            <div>Placement</div>
-                            <div>Creature</div>
-                            <div>Points</div>
-                        </div>
-                        <div id="creatureScoreboardStats">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-    </main>
-       `
 })
 
 function getRandomColorGenerator() { //generera en färg till monster
@@ -85,10 +36,8 @@ function getRandomColorGenerator() { //generera en färg till monster
     colorBar1.style.backgroundColor = `${color}`
 }
 
-function randomPictureGenerator() {
+function randomPictureGenerator() { //generera en random monster URL
     let monsterPicNum = Math.ceil(Math.random() * 3)
-    console.log(monsterPicNum)
-
     return picUrl = `../images/monster${monsterPicNum}.png`
 }
 selectRandomButton.addEventListener("click", () => {
@@ -108,4 +57,4 @@ getRandomColorGenerator();
 //1. koppla monster till card, göra en klass?
 //2. söka på monsters id 
 //3. koppla samman färger
-//4. koppla samman grafen
+//4. koppla samman grafen, medelvärde av poängen baserat på hur många gånger de tävlat alla tävlor olika måpnga gånger
