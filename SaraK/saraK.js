edde55b054ea04ceaec81da989d6c551e8615654
lelle1000 <linusestrandberg@gmail.com>
2026-05-22 12:28:04 +0200

    Fixed bug

--- SaraK/saraK.js
+++ SaraK/saraK.js
@@ -29,7 +29,7 @@ function colorGenerator() { //generera en färg till monster
 
 function randomPictureGenerator() { //generera en random monster URL
     let monsterPicNum = Math.ceil(Math.random() * 16)
-    return `../images/monster${monsterPicNum}.png`
+    return `./images/monster${monsterPicNum}.png`
 }
 
 
